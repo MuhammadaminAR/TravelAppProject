cd90eba523a85e60d9dcdc787f5097a7305f10e7
Zafar <a.b@gmail.com>
2025-03-31 13:06:59 +0500

    additionals

--- src/main/java/org/example/travelappproject/controller/LoginController.java
+++ src/main/java/org/example/travelappproject/controller/LoginController.java
@@ -3,14 +3,13 @@ package org.example.travelappproject.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.example.travelappproject.dto.LoginDTO;
+import org.example.travelappproject.dto.RegisterDTO;
 import org.example.travelappproject.service.LoginService;
+import org.example.travelappproject.service.RegisterService;
 import org.springframework.http.HttpEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
